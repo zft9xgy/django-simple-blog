64f2e4140d26fe5bb92f0aefd98520528c7fb778
zft9xgy <zft9xgy@icloud.com>
2024-06-28 13:42:42 +0200

    Change static config again

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -140,10 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
