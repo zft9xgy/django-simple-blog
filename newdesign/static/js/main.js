551e3489884737d244d791f7f200488c79ad29ee
zft9xgy <zft9xgy@proton.me>
2024-07-17 00:28:51 +0200

    Playing with dark mode prefs

--- newdesign/static/js/main.js
+++ newdesign/static/js/main.js
@@ -1,25 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeToggle = document.querySelector('.dark-mode');
-    const darkModeLinkId = 'dark-mode-css';
+const toggle = document.querySelector('.dark-mode');
 
+toggle.addEventListener('click', function() {
+  const isCurrentlyDarkMode = document.documentElement.classList.contains('dark');
 
-    darkModeToggle.addEventListener('click', () => {
-        let darkModeLink = document.getElementById(darkModeLinkId);
-
-        window.localStorage.setItem('theme', 'light');
-        if (darkModeLink) {
-            // Si el enlace ya existe, eliminarlo
-            darkModeLink.parentNode.removeChild(darkModeLink);
-        } else {
-            // Si el enlace no existe, añadirlo
-            darkModeLink = document.createElement('link');
-            darkModeLink.id = darkModeLinkId;
-            darkModeLink.rel = 'stylesheet';
-            darkModeLink.href = './static/styles/dark.css';
-            document.head.appendChild(darkModeLink);
-            window.localStorage.setItem('theme', 'dark');
-        }
-    });
-
-    
+  if (isCurrentlyDarkMode) {
+    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.add('light');
+    localStorage.setItem('theme', 'light');
+  } else {
+    document.documentElement.classList.add('dark');
+    document.documentElement.classList.remove('light');
+    localStorage.setItem('theme', 'dark');
+  }
 });
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+  document.documentElement.classList.add('dark');
+  document.documentElement.classList.remove('light');
+} else if (savedTheme === 'light') {
+  document.documentElement.classList.remove('dark'); 
+  document.documentElement.classList.add('light');
+ 
+}
+
+
+// Check to see if Media-Queries are supported
+if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      // Dark
+      console.log('dark')
+    } else {
+      // Light
+      console.log('light')
+    }
+  } else {
+    // Default (when Media-Queries are not supported)
+    console.log('default')
+  }
