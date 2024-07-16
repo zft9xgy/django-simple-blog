5009b6810fe811cf64f0a06ff41f20800ad42ae5
zft9xgy <zft9xgy@icloud.com>
2024-07-16 14:58:50 +0200

    Playing with darkmode, not finished

---
+++ newdesign/static/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.querySelector('.dark-mode');
+    const darkModeLinkId = 'dark-mode-css';
+
+    console.log('dark mode ok')
+
+    darkModeToggle.addEventListener('click', () => {
+        let darkModeLink = document.getElementById(darkModeLinkId);
+
+        if (darkModeLink) {
+            // Si el enlace ya existe, eliminarlo
+            darkModeLink.parentNode.removeChild(darkModeLink);
+        } else {
+            // Si el enlace no existe, añadirlo
+            darkModeLink = document.createElement('link');
+            darkModeLink.id = darkModeLinkId;
+            darkModeLink.rel = 'stylesheet';
+            darkModeLink.href = './static/styles/dark.css';
+            document.head.appendChild(darkModeLink);
+        }
+    });
+});
